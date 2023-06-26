6c5dd7a502a401da801ba9ccfecb9c63c82743e4
Perfight <m0nter1327@mail.ru>
2023-06-26 10:09:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calc/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.calc;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        TextView text = findViewById(R.id.text);
+        text.setText(bundle.getString("key"));
+    }
+}
